ae49c25d1bdd8bc68f932637b72170ede7e4c829
TimTimTstuff <timo.kesselring@tstuff.de>
2020-05-11 16:08:59 +0200

    clean package

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
 import * as PIXI from 'pixi.js'
 import './style/main.css'
 import './style/main.css'
-import {GameLoop, FPSReport} from '@timtimtstuff/tstuffgametools/distj'
+import {GameLoop, FPSReport, GameEventRegister, GameLoopEventRegister} from '@timtimtstuff/tstuffgametools/distj'
 
 const app = new PIXI.Application({
     width: 800,
     height: 600,
     backgroundColor: 0x7AD9B5
 })
-
-const x = new GameLoop()
-const fps = new FPSReport(20)
-x.addUpdate(()=>{
-    console.log(fps.calculateFps(GameLoop.deltaTime))
-})
-
-x.start()
-document.getElementById('game-area')?.appendChild(app.view)
-setTimeout(() => {
-    x.stop()
-}, 3000);
