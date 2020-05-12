a1d15972b92b7ab6fdaf461f41805dd3bfb08afa
TimTimTstuff <timo.kesselring@tstuff.de>
2020-05-12 07:00:55 +0200

    implement graphics test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
 import * as PIXI from 'pixi.js'
 import './style/main.css'
 import './style/main.css'
-import {GameLoop, FPSReport, GameEventRegister, GameLoopEventRegister} from '@timtimtstuff/tstuffgametools/distj'
+import { GameContext } from './global/GameContext'
 
-const app = new PIXI.Application({
+const ctx = new GameContext(new PIXI.Application({
     width: 800,
     height: 600,
     backgroundColor: 0x7AD9B5
+}), 'browsergame')
+
+let sprite : PIXI.Sprite 
+document.getElementById('game-area')?.appendChild(ctx.canvasApp.view)
+PIXI.Loader.shared
+    .add('test','../assets/textures/factory.png')
+    .load((l,r)=> {
+        sprite = new PIXI.Sprite(r.test?.texture)
+
+        GameContext.instance.canvasApp.stage.addChild(sprite)
+        ctx.startGame()
+    })
+let count = 0
+ctx.gameLoop.addUpdate(() => {
+    count++
+    sprite.scale.set(0.1,0.1)
+    if(count > 0){
+    sprite.position.set(sprite.position.x+1,sprite.position.y+1)
+    }else{
+        sprite.position.set(sprite.position.x-1,sprite.position.y-1) 
+    }
+
+    if(count > 300) {
+        count = -300
+    }
+    console.log('move')
 })
+
+
